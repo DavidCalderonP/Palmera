cbc4af0b4b9f1fa8ce2e146001b11d6cbb4ca125
KevinTortoledoRodriguez <60956746+KevinTortoledoRodriguez@users.noreply.github.com>
2021-10-11 22:39:15 -0600

    Correccion de errores o.o

--- resources/views/predios/needLogin.blade.php
+++ resources/views/predios/needLogin.blade.php
@@ -1,4 +1,4 @@
 <script type="aplication/javascript">
   alert("Es necesario iniciar sesión")
   window.location.replace("http://127.0.0.1:8000/login")
-<script>
+</script>
--- resources/views/predios/predioDoesntExist.blade.php
+++ resources/views/predios/predioDoesntExist.blade.php
@@ -1,4 +1,4 @@
 <script type="aplication/javascript">
   alert("El predio que selecciono ya no existe")
   window.location.replace("http://127.0.0.1:8000/predio")
-<script>
+</script>
